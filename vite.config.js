5123e09a26ed4856b469bb3f7211ac22bb73a248
Vsevolod Kochnev <v.kochnev@sports.ru>
2022-12-12 21:53:58 +0300

    add build

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.TARGET_ENV === 'staging'
-    ? '/demo/special-vite-template/'
-    : '/special-vite-template/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
